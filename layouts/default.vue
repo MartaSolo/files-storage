b201030091b86db52fc2136c781a76f0515662e4
Marta Solomacha <marta.solomacha@monterail.com>
2023-02-05 19:15:33 +0100

    Add layouts folder and default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="flex justify-center shadow-sm">
+    <h1 class="font-mono text-2xl my-4">Nuxt 3 Playground</h1>
+  </header>
+
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style lang="css" scoped>
+
+</style>
