ab67c1fbbeb4d53f504dfcf0dbaa4523d09e85c2
Marta Solomacha <marta.solomacha@monterail.com>
2023-02-26 15:40:55 +0100

    Add vite and css to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: "@import '@/assets/scss/_colors.scss';",
+        },
+      },
+    },
+  },
+  css: ["@/assets/scss/main.scss"],
 });
+
+// export default defineNuxtConfig({
+//   modules: ["@nuxtjs/tailwindcss"],
+//   // buildModules: ["@nuxtjs/style-resources"],
+//   // styleResources: {
+//   //   scss: [
+//   //     "./assets/scss/main.scss",
+//   //     "./assets/scss/_colors.scss",
+//   //     "./assets/scss/_font.scss",
+//   //   ],
+//   //   hoistUseStatements: true,
+//   // },
+// });
