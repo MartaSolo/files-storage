9b919a974641d0ad52737c99a8159447a9fec206
Marta Solomacha <marta.solomacha@monterail.com>
2023-03-19 22:11:13 +0100

    Add Header component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,7 @@
 
 <template>
   <div>
-    <header class="flex justify-center shadow-sm">
-      <h1 class="font-mono text-2xl my-4">Nuxt 3 Playground</h1>
-    </header>
-
+    <Header />
     <main class="content">
       <slot />
     </main>
