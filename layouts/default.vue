01d64e195588160e1398c461ba4830ae67809770
Marta Solomacha <marta.solomacha@monterail.com>
2023-03-25 19:01:28 +0100

    Edit default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const tabs: { label: string; path: string }[] = [
+  {
+    label: "Add new files",
+    path: "/",
+  },
+  {
+    label: "All your files",
+    path: "/allYourFiles",
+  },
+];
+</script>
 
 <template>
   <div>
     <TheHeader />
+    <BaseTabs :tabs="tabs" />
     <main class="content">
       <slot />
     </main>
