892421de687aa2173be9942d27291ef897a471ef
Marta Solomacha <marta.solomacha@monterail.com>
2023-03-25 19:00:51 +0100

    Add BaseTabs component

---
+++ components/BaseTabs.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  tabs: { label: string; path: string }[];
+}>();
+</script>
+
+<template>
+  <ul class="tabs" role="tablist">
+    <BaseTab v-for="tab in tabs" :key="tab.path" :tab="tab" />
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.tabs {
+  display: flex;
+}
+</style>
