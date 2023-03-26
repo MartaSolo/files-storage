016b7115b92804f0c070587d6cf4773759ec30d1
Marta Solomacha <marta.solomacha@monterail.com>
2023-03-26 22:01:27 +0200

    Add StorageFileList component

---
+++ components/StorageFileListItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { FileObject } from "@/types/FileObject";
+
+const props = defineProps<{
+  file: FileObject;
+}>();
+</script>
+
+<template>
+  <li class="file">{{ file.name }}</li>
+</template>
+
+<style lang="scss" scoped></style>
