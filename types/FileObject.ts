6f92e985dd36f47be04ba8f50a28393cb48b3f15
Marta Solomacha <marta.solomacha@monterail.com>
2023-04-02 11:28:36 +0200

    Edit FileObject interface

--- types/FileObject.ts
+++ types/FileObject.ts
@@ -1,4 +1,3 @@
-// works
 export interface Metadata {
   cacheControl: string;
   contentLength: number;
@@ -15,47 +14,5 @@ export interface FileObject {
   last_accessed_at: string;
   name: string;
   updated_at: string;
-  metadata: {
-    [key: string]: Metadata;
-  };
+  metadata: Metadata;
 }
-
-// doesn't work
-
-// export interface Metadata {
-//   cacheControl: string;
-//   contentLength: number;
-//   eTag: string;
-//   httpStatusCode: number;
-//   lastModified: string;
-//   mimetype: string;
-//   size: number;
-// }
-
-// export interface FileObject {
-//   created_at: string;
-//   id: string;
-//   last_accessed_at: string;
-//   name: string;
-//   updated_at: string;
-//   metadata: Metadata;
-// }
-
-// doesn't work
-
-// export interface FileObject {
-//   created_at: string;
-//   id: string;
-//   last_accessed_at: string;
-//   name: string;
-//   updated_at: string;
-//   metadata: {
-//     cacheControl: string;
-//     contentLength: number;
-//     eTag: string;
-//     httpStatusCode: number;
-//     lastModified: string;
-//     mimetype: string;
-//     size: number;
-//   };
-// }
