acee6ad08d1b859bb29d9884898488f948593052
Marta Solomacha <marta.solomacha@monterail.com>
2023-04-08 21:06:01 +0200

    Edit SortFilesSelect_2

--- components/SortFilesSelect_2.vue
+++ components/SortFilesSelect_2.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { SortOption } from "@/types/SortOptions";
+import SortUp from "@/components/svg/SortUp.vue";
+import SortDown from "@/components/svg/SortDown.vue";
 
 const sortOptions = ref<SortOption[]>([
   { name: "name ascending", column: "name", order: "asc" },
@@ -16,11 +18,15 @@ const defaultSelectedOption = sortOptions.value[0].name;
 
 const selectedOption = ref<string>(defaultSelectedOption);
 
-const isDropdownOpen = ref(true);
+const isDropdownOpen = ref(false);
 
 const emit = defineEmits<{
   (e: "setSortOptions", column: string, order: string): void;
 }>();
+
+const selectedClass = (name: string) => {
+  return name === selectedOption.value ? "sort__option--selected" : "";
+};
 
 const toggleOptions = () => {
   isDropdownOpen.value = !isDropdownOpen.value;
@@ -31,21 +37,34 @@ const handleClick = (chosenOption: SortOption) => {
   selectedOption.value = chosenOption.name;
   emit("setSortOptions", chosenOption.column, chosenOption.order);
 };
+
+const selectedOnKeyDown = (e: KeyboardEvent) => {
+  if (e.key === "Enter") {
+    toggleOptions();
+  }
+};
 </script>
 
 <template>
-  <div class="select">
-    <label class="select__title">Sort by:</label>
-    <input
-      v-model="selectedOption"
-      class="select__option"
-      @focus="isDropdownOpen = true"
-    />
-    <ul v-if="isDropdownOpen" class="select__list">
+  <div class="sort">
+    <div class="sort__select">
+      <div class="sort__label">Sort by:</div>
+      <div
+        class="sort__selected"
+        tabindex="0"
+        @click="toggleOptions"
+        @keydown="selectedOnKeyDown"
+      >
+        <div class="sort__selected__option">{{ selectedOption }}</div>
+        <component :is="isDropdownOpen ? SortUp : SortDown" />
+      </div>
+    </div>
+    <ul v-if="isDropdownOpen" class="sort__list">
       <li
         v-for="option in sortOptions"
         :key="option.name"
-        class="select__option select__option--dropdown"
+        class="sort__option"
+        :class="selectedClass(option.name)"
         tabindex="0"
         @click="handleClick(option)"
       >
@@ -56,37 +75,49 @@ const handleClick = (chosenOption: SortOption) => {
 </template>
 
 <style lang="scss" scoped>
-.select {
-  display: flex;
-}
-
-.select__title {
-  padding: 0.5rem 0.5rem 0 0;
-}
-
-.select__list {
+.sort {
   position: relative;
 }
 
-.select__option {
-  border: 1px solid rgb(241, 238, 238);
-  border-radius: 8px;
-  padding: 0.5rem;
-  width: 210px;
-  background-color: $color-white;
-}
-.select__option--dropdown {
-  border: 1px solid rgb(241, 238, 238);
-  padding: 0.5rem;
-  width: 210px;
-  position: absolute;
-  background-color: $color-white;
+.sort__select {
+  display: inline-flex;
 }
 
-@for $i from 1 through 8 {
-  .select__option:nth-child(#{$i}) {
-    top: 42px * ($i);
-    // top: 42px * ($i - 1);
+.sort__label {
+  padding: 0.5rem 0.5rem 0 0;
+}
+
+.sort__selected {
+  border: 1px solid $color-green-light;
+  border-radius: 8px;
+  padding: 0.5rem;
+  width: 230px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.sort__list {
+  border: 1px solid $color-green-light;
+  border-radius: 8px;
+  width: 230px;
+  background-color: $color-white;
+  position: absolute;
+  left: 66px;
+}
+
+.sort__option {
+  padding: 0.5rem;
+}
+
+@for $i from 1 through 7 {
+  .sort__option:nth-child(#{$i}) {
+    border-bottom: 1px solid $color-green-light;
   }
 }
+
+.sort__option--selected {
+  font-weight: 800;
+}
 </style>
+
