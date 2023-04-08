9b0fb5b1767254dd8954a6ebcf6a08a92e6a5277
Marta Solomacha <marta.solomacha@monterail.com>
2023-04-08 21:06:13 +0200

    Edit SortFilesSelect

--- components/SortFilesSelect.vue
+++ components/SortFilesSelect.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { SortOptions, Keys } from "@/types/SortOptions";
+import { SortOption } from "@/types/SortOptions";
+import SortUp from "@/components/svg/SortUp.vue";
+import SortDown from "@/components/svg/SortDown.vue";
 
-const isDropdownOpen = ref(true);
-
-const sortOptions = ref<SortOptions[]>([
+const sortOptions = ref<SortOption[]>([
   { name: "name ascending", column: "name", order: "asc" },
   { name: "name descending", column: "name", order: "desc" },
   { name: "type ascending", column: "mimetype", order: "asc" },
@@ -14,73 +14,105 @@ const sortOptions = ref<SortOptions[]>([
   { name: "time created descending", column: "created_at", order: "desc" },
 ]);
 
+const defaultSelectedOption = sortOptions.value[0].name;
+
+const selectedOption = ref<string>(defaultSelectedOption);
+
+const isDropdownOpen = ref(false);
+
 const emit = defineEmits<{
-  (e: "setSortOptions", column: Keys, order: Keys): void;
+  (e: "setSortOptions", column: string, order: string): void;
 }>();
 
-const dropdownSortOptions = computed(() => {
-  return sortOptions.value.slice(1);
-});
+const selectedClass = (name: string) => {
+  return name === selectedOption.value ? "sort__option--selected" : "";
+};
 
 const toggleOptions = () => {
   isDropdownOpen.value = !isDropdownOpen.value;
 };
 
-const handleClick = (column: Keys, order: Keys) => {
+const handleClick = (chosenOption: SortOption) => {
   toggleOptions();
-  emit("setSortOptions", column, order);
+  selectedOption.value = chosenOption.name;
+  emit("setSortOptions", chosenOption.column, chosenOption.order);
+};
+
+const selectedOnKeyDown = (e: KeyboardEvent) => {
+  if (e.key === "Enter") {
+    toggleOptions();
+  }
 };
 </script>
 
 <template>
-  <div class="select">
-    <div class="select__title">Sort by:</div>
-    <ul class="select__list">
-      <li
-        class="select__option"
+  <div class="sort">
+    <div class="sort__select">
+      <div class="sort__label">Sort by:</div>
+      <div
+        class="sort__selected"
         tabindex="0"
-        @click="handleClick(sortOptions[0].column, sortOptions[0].order)"
+        @click="toggleOptions"
+        @keydown="selectedOnKeyDown"
       >
-        {{ sortOptions[0].name }}
+        <div class="sort__selected__option">{{ selectedOption }}</div>
+        <component :is="isDropdownOpen ? SortUp : SortDown" />
+      </div>
+    </div>
+    <ul v-if="isDropdownOpen" class="sort__list">
+      <li
+        v-for="option in sortOptions"
+        :key="option.name"
+        class="sort__option"
+        :class="selectedClass(option.name)"
+        tabindex="0"
+        @click="handleClick(option)"
+      >
+        {{ option.name }}
       </li>
-      <template v-if="isDropdownOpen">
-        <li
-          v-for="option in dropdownSortOptions"
-          :key="option.name"
-          class="select__option"
-          tabindex="0"
-          @click="handleClick(option.column, option.order)"
-        >
-          {{ option.name }}
-        </li>
-      </template>
     </ul>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.select {
-  display: flex;
-}
-
-.select__title {
-  padding: 0.5rem 0.5rem 0 0;
-}
-.select__list {
+.sort {
   position: relative;
 }
-
-.select__option {
-  border: 1px solid grey;
-  padding: 0.5rem;
-  width: 210px;
-  position: absolute;
-  background-color: $color-white;
+.sort__select {
+  display: inline-flex;
+}
+.sort__label {
+  padding: 0.5rem 0.5rem 0 0;
 }
 
-@for $i from 2 through 8 {
-  .select__option:nth-child(#{$i}) {
-    top: 42px * ($i - 1);
+.sort__selected {
+  border: 1px solid $color-green-light;
+  border-radius: 8px;
+  padding: 0.5rem;
+  width: 230px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.sort__list {
+  border: 1px solid $color-green-light;
+  border-radius: 8px;
+  width: 230px;
+  background-color: $color-white;
+  position: absolute;
+  left: 66px;
+}
+.sort__option {
+  padding: 0.5rem;
+}
+
+@for $i from 1 through 7 {
+  .sort__option:nth-child(#{$i}) {
+    border-bottom: 1px solid $color-green-light;
   }
+}
+.sort__option--selected {
+  font-weight: 800;
 }
 </style>
