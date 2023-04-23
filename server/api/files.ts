ecbcb9847ce24585f466e77c475a705ebc2d4ec0
Marta Solomacha <marta.solomacha@monterail.com>
2023-04-23 12:03:04 +0200

    Add typed for query in files.ts

--- server/api/files.ts
+++ server/api/files.ts
@@ -1,4 +1,5 @@
 import { FileObject } from "@supabase/storage-js";
+import { FileObjectKeys } from "~~/types/FileObjectKeys";
 import { serverSupabaseClient } from "#supabase/server";
 
 export default defineEventHandler(async (event) => {
@@ -13,12 +14,17 @@ export default defineEventHandler(async (event) => {
 
   const query = getQuery(event);
 
+  const { key, order } = query as {
+    key: FileObjectKeys;
+    order: "asc" | "desc";
+  };
+
   // sort fetched data
   data.sort((e1: FileObject, e2: FileObject) => {
-    const a = e1[query.key] ? e1[query.key] : e1.metadata[query.key];
-    const b = e2[query.key] ? e2[query.key] : e2.metadata[query.key];
-    const sortOrder = query.order === "asc" ? 1 : -1;
-    if (query.key === "name") {
+    const a = e1[key] ? e1[key] : e1.metadata[key];
+    const b = e2[key] ? e2[key] : e2.metadata[key];
+    const sortOrder = order === "asc" ? 1 : -1;
+    if (key === "name") {
       return sortOrder === 1 ? a.localeCompare(b) : b.localeCompare(a);
     }
     return a < b ? -sortOrder : a > b ? sortOrder : 0;
