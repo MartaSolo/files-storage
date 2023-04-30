cc8ed1e88ef8abebc3d69bc0a72e37ac14bbb565
Marta Solomacha <marta.solomacha@monterail.com>
2023-04-30 22:11:31 +0200

    Add toggling layout

--- components/StorageFileList.vue
+++ components/StorageFileList.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { FileObject } from "@supabase/storage-js";
+import GridLayout from "@/components/svg/GridLayout.vue";
+import ListLayout from "@/components/svg/ListLayout.vue";
 
 const sortColumn = useSortColumn();
 const sortOrder = useSortOrder();
+const fileView = useFileView();
 
 const {
   data: fileList,
@@ -14,6 +17,12 @@ const {
 
 const sortFiles = () => {
   refresh();
+};
+
+const toggleLayout = () => {
+  fileView.value === "grid"
+    ? (fileView.value = "list")
+    : (fileView.value = "grid");
 };
 </script>
 
@@ -27,6 +36,7 @@ const sortFiles = () => {
     />
     <template v-else>
       <SortFileList @set-sort-options="sortFiles" />
+      <button class="files__view" @click="toggleLayout"></button>
       <h2 class="files__title">Your uploaded files:</h2>
       <ul class="files__list">
         <StorageFileListItem
@@ -39,4 +49,10 @@ const sortFiles = () => {
   </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.files__view {
+  width: 50px;
+  height: 50px;
+  background-color: green;
+}
+</style>
