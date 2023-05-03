3529e38975c923fab97cd0d0cf04a3939b9796d7
Marta Solomacha <marta.solomacha@monterail.com>
2023-05-03 12:18:48 +0200

    Edit composable name

--- composables/useState.ts
+++ composables/useState.ts
@@ -2,4 +2,4 @@ export const useSortColumn = () => useState("sort-column", () => "name");
 
 export const useSortOrder = () => useState("sort-order", () => "asc");
 
-export const useFileView = () => useState("file-view", () => "grid");
+export const useLayoutType = () => useState("layout-type", () => "grid");
