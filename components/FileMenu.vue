dddcd6d770697cef08929f8582b50e8b7fc7c4d2
Marta Solomacha <marta.solomacha@monterail.com>
2023-05-06 18:19:46 +0200

    Use IconButton in FileMenu

--- components/FileMenu.vue
+++ components/FileMenu.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import MoreActions from "@/components/svg/MoreActions.vue";
 
-const { isTextDisplayed, showText, hideText } = useButtonText();
-
 const isMenuOpen = ref(false);
 
 const root = ref<HTMLElement | null>(null);
@@ -13,24 +11,16 @@ useClickOutside(root, () => {
 
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
-  if (isTextDisplayed) isTextDisplayed.value = false;
 };
 </script>
 
 <template>
   <div ref="root" class="menu">
-    <button
-      class="menu__button"
-      aria-labelledby="description"
-      @click="toggleMenu"
-      @mouseenter="showText"
-      @mouseleave="hideText"
-    >
-      <MoreActions />
-    </button>
-    <span v-show="isTextDisplayed" id="description" class="menu__description"
-      >More actions</span
-    >
+    <IconButton description="More actions" theme="green" @click="toggleMenu">
+      <template #icon>
+        <MoreActions />
+      </template>
+    </IconButton>
     <div v-if="isMenuOpen" class="menu__list"></div>
   </div>
 </template>
@@ -42,25 +32,7 @@ const toggleMenu = () => {
   height: 40px;
   position: relative;
 }
-.menu__button {
-  border-radius: 50%;
-  background-color: $color-green-light;
-  width: 40px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  &:hover {
-    background-color: $color-green-medium;
-  }
-}
 
-.menu__description {
-  @include buttonDescription;
-  top: 40px;
-  left: -22px;
-  width: 90px;
-}
 .menu__list {
   width: 200px;
   height: 200px;
