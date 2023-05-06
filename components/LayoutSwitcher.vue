85edf25e9eae4094287649985436736a7482955c
Marta Solomacha <marta.solomacha@monterail.com>
2023-05-06 18:20:09 +0200

    Use IconButton in LayoutSwitcher

--- components/LayoutSwitcher.vue
+++ components/LayoutSwitcher.vue
@@ -1,10 +1,8 @@
-<script setup>
+<script setup lang="ts">
 import GridLayout from "@/components/svg/GridLayout.vue";
 import ListLayout from "@/components/svg/ListLayout.vue";
 
 const layoutType = useLayoutType();
-
-const { isTextDisplayed, showText, hideText } = useButtonText();
 
 const descriptionText = computed(() => {
   return layoutType.value === "grid" ? "Grid layout" : "List layout";
@@ -22,46 +20,11 @@ const toggleLayout = () => {
 </script>
 
 <template>
-  <div class="layout">
-    <button
-      class="layout__button"
-      aria-labelledby="description"
-      @click="toggleLayout"
-      @mouseenter="showText"
-      @mouseleave="hideText"
-    >
+  <IconButton :description="descriptionText" theme="grey" @click="toggleLayout">
+    <template #icon>
       <component :is="layoutComponent" />
-    </button>
-    <span v-show="isTextDisplayed" id="description" class="layout__text">
-      {{ descriptionText }}
-    </span>
-  </div>
+    </template>
+  </IconButton>
 </template>
 
-<style lang="scss" scoped>
-.layout {
-  position: relative;
-  width: 80px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.layout__button {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  background-color: $color-grey-lighter;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.layout__button:hover {
-  background-color: $color-grey-light;
-}
-
-.layout__text {
-  @include buttonDescription;
-  top: 52px;
-}
-</style>
+<style lang="scss" scoped></style>
