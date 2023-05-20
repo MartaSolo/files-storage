a0d152fbb204ef4b4b34c7c3fb40ee0396d990d1
Marta Solomacha <marta.solomacha@monterail.com>
2023-05-20 16:58:37 +0200

    Add useCopyFile composable

---
+++ composables/useCopyFile.ts
@@ -0,0 +1,18 @@
+export const useCopyFile = (fileName: String) => {
+  const client = useSupabaseClient();
+
+  const copyName = useFileName(fileName);
+
+  const copy = async () => {
+    const { data, error } = await client.storage
+      .from("files")
+      .copy(
+        `public/${fileName}`,
+        `public/${copyName.name}_Copy${copyName.extension}`
+      );
+    if (error) throw error;
+    return data;
+  };
+
+  return { copy };
+};
