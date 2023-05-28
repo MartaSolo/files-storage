f6439517b7e17a8bd9735b09c57ead0ac183621f
Marta Solomacha <marta.solomacha@monterail.com>
2023-05-28 22:24:43 +0200

    Add BaseButton component

---
+++ components/BaseButton.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: String;
+  theme: "white" | "green";
+}>();
+
+const computedClass = computed(() => {
+  return ["button", `button--${props.theme}`];
+});
+</script>
+
+<template>
+  <button :class="computedClass">{{ label }}</button>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 0.75rem;
+  border-radius: 40px;
+  min-width: 90px;
+}
+
+.button--white {
+  background-color: $color-white;
+  color: $color-green-dark;
+  &:hover {
+    background-color: $color-grey-lightest;
+  }
+}
+.button--green {
+  background-color: $color-green-dark;
+  color: $color-white;
+  &:hover {
+    background-color: $color-green-dark-hover;
+  }
+}
+</style>
