44cf0a34037d083273a6278bc8951138ca84ba8b
Marta Solomacha <marta.solomacha@monterail.com>
2023-06-17 15:13:44 +0200

    Edit useDeleteFile

--- composables/useDeleteFile.ts
+++ composables/useDeleteFile.ts
@@ -1,19 +1,10 @@
-export const useDeleteFile = (fileName?: String, fileNames?: string[]) => {
+export const useDeleteFile = () => {
   const client = useSupabaseClient();
 
-  let files: string[];
-
-  if (fileName) {
-    files = [`public/${fileName}`];
-  } else {
-    fileNames?.forEach((file) => {
-      const path = `public/${file}`;
-      files.push(path);
-    });
-  }
-
-  const remove = async () => {
-    const { data, error } = await client.storage.from("files").remove(files);
+  const remove = async (fileName: string) => {
+    const { data, error } = await client.storage
+      .from("files")
+      .remove([`public/${fileName}`]);
     if (error) throw error;
     return data;
   };
