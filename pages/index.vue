711a96577e4c69fa0b5c1ae299b72b77d31f29f2
Marta Solomacha <marta.solomacha@monterail.com>
2023-07-31 21:50:10 +0200

    Replace numbers with constants in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { MAX_FILE_SIZE_MB } from "@/utils/constants/maxFileSizeMB";
+import { MAX_FILES_NUMBER } from "@/utils/constants/maxFilesNumber";
+</script>
 
 <template>
   <div>
-    <DragAndDropInput :max-file-size-m-b="5" :max-files-number="10" />
+    <DragAndDropInput
+      :max-file-size-m-b="MAX_FILE_SIZE_MB"
+      :max-files-number="MAX_FILES_NUMBER"
+    />
   </div>
 </template>
 
