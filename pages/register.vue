8dacd410484bbbfcb1a90143d5de939496b88734
Marta Solomacha <marta.solomacha@monterail.com>
2023-08-16 22:44:12 +0200

    Add register page

--- pages/register.vue
+++ pages/register.vue
@@ -3,14 +3,80 @@ definePageMeta({
   layout: "login-layout",
 });
 
-const name = ref<string>("");
-const email = ref<string>("");
-const password = ref<string>("");
+const EyeIcon = resolveComponent("EyeIcon");
+
+const { validateName, nameError } = useValidateName();
+const { validateEmail, emailError } = useValidateEmail();
+const { validatePassword, passwordError } = useValidatePassword();
+const { register } = useCreateUser();
+const isUserLoggedIn = useIsUserLoggedIn();
+
+const name = ref("");
+const email = ref("");
+const password = ref("");
+const bucket = ref("");
+const passwordInputType = ref("password");
+const registerError = ref("");
+
+const handleNameFocus = () => {
+  if (nameError.value) nameError.value = "";
+};
+
+const handleNameBlur = () => {
+  console.log("handleNameBlur fired");
+  validateName(name.value);
+};
+
+const handleEmailFocus = () => {
+  if (emailError.value) emailError.value = "";
+};
+
+const handlePasswordFocus = () => {
+  if (passwordError.value) passwordError.value = "";
+};
+
+const validationClasses = (error: string, inputValue: string) => {
+  return [error ? "error" : "", inputValue && !error ? "success" : ""];
+};
+
+const toggleShowPassword = () => {
+  return passwordInputType.value === "password"
+    ? (passwordInputType.value = "text")
+    : (passwordInputType.value = "password");
+};
+
+const validateForm = () => {
+  return (
+    validateName(name.value) &&
+    validateEmail(email.value) &&
+    validatePassword(password.value)
+  );
+};
+
+const registerUser = async () => {
+  try {
+    await register(name.value, email.value, password.value);
+    isUserLoggedIn.value = true;
+  } catch (error: any) {
+    registerError.value = error.message;
+  }
+};
+
+// utworzenie buycketa??
+
+const handleSubmit = () => {
+  if (!validateForm()) return;
+  console.log("handleSubmit");
+  registerUser();
+  // if (!registerError.value) {
+    // createBucket();
+  // }
+};
 </script>
 
 <template>
-  <section class="register">
-    <h2 class="register__title">Register to create your private storage.</h2>
+  <form class="register" @submit.prevent="handleSubmit">
+    <h2 class="register__title">Register and create your private storage.</h2>
     <div class="register__content">
       <div class="register__inputs">
         <BaseInput
@@ -18,26 +84,69 @@ const password = ref<string>("");
           name="name"
           label="Name"
           class="register__name"
+          :class="validationClasses(nameError, name)"
+          @focus="handleNameFocus"
+          @blur="handleNameBlur"
         />
+        <span v-if="nameError" class="error__message">{{ nameError }}</span>
         <BaseInput
           v-model="email"
           name="email"
           label="Email"
           class="register__email"
+          :class="validationClasses(emailError, email)"
+          @focus="handleEmailFocus"
+          @blur="validateEmail(email)"
+        />
+        <span v-if="emailError" class="error__message">{{ emailError }}</span>
+        <BaseAccordion
+          title="Password hint"
+          content="Password must consist of 8 to 15 characters and contain at least one
+          lowercase letter, one uppercase letter, one numeric digit, and one
+          special character."
         />
         <BaseInput
           v-model="password"
+          :type="passwordInputType"
           name="password"
           label="Password"
           class="register__password"
+          :class="validationClasses(passwordError, password)"
+          @focus="handlePasswordFocus"
+          @blur="validatePassword(password)"
+          ><button
+            type="button"
+            class="register__password--button"
+            @click="toggleShowPassword"
+          >
+            <EyeIcon />
+          </button>
+        </BaseInput>
+        <span v-if="passwordError" class="error__message">{{
+          passwordError
+        }}</span>
+        <BaseAccordion
+          title="Storage name hint"
+          content="Storage name can contain lowercase and uppercase letters, numbers and some special characters: exclamation point (!), hyphen (-), underscore (_), period (.), asterisk (*), single quote ('), open parenthesis ((), close parenthesis ())."
         />
+        <!-- <BaseInput
+          v-model="bucket"
+          name="bucket"
+          label="Private storage name"
+          class="register__bucket"
+          :class="validationClasses(bucketError, bucket)"
+          @focus="handleBucketFocus"
+          @blur="validateBucket(bucket)"
+        /> -->
+        <!-- <span v-if="nameError" class="error__message">{{ nameError }}</span> -->
       </div>
       <div class="register__actions">
         <BaseButton label="Cancel" theme="white" to="/" />
-        <BaseButton label="Confirm" />
+        <BaseButton type="submit" label="Confirm" />
       </div>
+      <span v-if="registerError">{{ registerError }}</span>
     </div>
-  </section>
+  </form>
 </template>
 
 <style lang="scss" scoped>
@@ -62,10 +171,18 @@ const password = ref<string>("");
 
 .register__name,
 .register__email,
-.register__password {
+.register__password,
+.register__bucket {
   flex-direction: column;
   align-items: flex-start;
   padding-bottom: 2rem;
+}
+
+.error.register__name,
+.error.register__email,
+.error.register__password,
+.error.register__bucket {
+  padding-bottom: 0.2rem;
 }
 
 .register__actions {
@@ -86,5 +203,52 @@ const password = ref<string>("");
 
 .register__redirect--link {
   width: 100%;
+}
+
+.error__message {
+  font-size: 0.8rem;
+  color: $text-color-error;
+  display: block;
+  padding-bottom: 0.6rem;
+}
+
+.register__password--info {
+  font-size: 0.9rem;
+  color: $color-green-dark-hover;
+  padding-bottom: 1rem;
+}
+
+.register__password--button {
+  border-radius: 8px;
+  position: absolute;
+  right: 7px;
+  top: 32px;
+}
+</style>
+
+<style lang="scss">
+.register__name.input__input,
+.register__email.input__input,
+.register__password.input__input,
+.register__bucket.input__input {
+  width: 100%;
+}
+
+.error.register__name.input__input,
+.error.register__email.input__input,
+.error.register__password.input__input,
+.error.register__bucket.input__input {
+  border: 1px solid $text-color-error;
+}
+
+.success.register__name.input__input,
+.success.register__email.input__input,
+.success.register__password.input__input,
+.success.register__bucket.input__input {
+  border: 1px solid $text-color-success;
+}
+
+.register__password {
+  position: relative;
 }
 </style>
