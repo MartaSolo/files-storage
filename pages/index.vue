30ded5ab3f4454596ce841544b13ad13d43879f1
Marta Solomacha <marta.solomacha@monterail.com>
2023-08-21 22:14:33 +0200

    Remove redundant div from index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,10 @@ import { MAX_FILES_NUMBER } from "@/utils/constants/maxFilesNumber";
 </script>
 
 <template>
-  <div>
-    <DragAndDropInput
-      :max-file-size-m-b="MAX_FILE_SIZE_MB"
-      :max-files-number="MAX_FILES_NUMBER"
-    />
-  </div>
+  <DragAndDropInput
+    :max-file-size-m-b="MAX_FILE_SIZE_MB"
+    :max-files-number="MAX_FILES_NUMBER"
+  />
 </template>
 
 <style lang="css" scoped></style>
