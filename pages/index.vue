b84a84f95288d19df82961d480b113c9a9a47ef1
Marta Solomacha <marta.solomacha@monterail.com>
2023-08-31 22:14:26 +0200

    Add getSession to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <script setup lang="ts">
 import { MAX_FILE_SIZE_MB } from "@/utils/constants/maxFileSizeMB";
 import { MAX_FILES_NUMBER } from "@/utils/constants/maxFilesNumber";
+
+const { retrieveSession } = useRetrieveSession();
+
+const getSessionError = ref("");
+
+const getSession = async () => {
+  try {
+    await retrieveSession();
+  } catch (error: any) {
+    getSessionError.value = error.message;
+  }
+};
+
+onMounted(() => {
+  getSession();
+});
 </script>
 
 <template>
