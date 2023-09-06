070f2d21613923b02abbb8c53d486e1bd9d79b1a
Marta Solomacha <marta.solomacha@monterail.com>
2023-09-06 22:25:03 +0200

    Add UserMenu component

---
+++ components/UserMenu.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const UserIcon = resolveComponent("UserIcon");
+</script>
+
+<template>
+  <button class="user">
+    <UserIcon />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.user {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid $color-green-dark;
+}
+</style>
