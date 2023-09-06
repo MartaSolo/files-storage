04623514a00fd868a0fc69fb850a42ad3380ac2a
Marta Solomacha <marta.solomacha@monterail.com>
2023-09-06 22:23:42 +0200

    Add UserData type

---
+++ types/UserData.ts
@@ -0,0 +1,7 @@
+export interface UserData {
+  id: string | undefined;
+  name: string | undefined;
+  email: string | undefined;
+  lastSignIn: string | undefined;
+  created: string | undefined;
+}
