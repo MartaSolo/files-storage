27a24f460f320adb61adfa4c0cd9826e1be093f1
Marta Solomacha <marta.solomacha@monterail.com>
2023-09-10 21:38:41 +0200

    Edit form on login page

--- pages/login.vue
+++ pages/login.vue
@@ -3,31 +3,108 @@ definePageMeta({
   layout: "login-layout",
 });
 
-const email = ref<string>("");
-const password = ref<string>("");
+const EyeIcon = resolveComponent("EyeIcon");
+
+const router = useRouter();
+const { validateEmail, emailError } = useValidateEmail();
+const { validatePassword, passwordError } = useValidatePassword();
+const { login } = useLoginUser();
+
+const email = ref("");
+const password = ref("");
+const passwordInputType = ref("password");
+const loginError = ref("");
+
+const isFormValid = computed(() => {
+  return !!(
+    email.value &&
+    password.value &&
+    !emailError.value &&
+    !passwordError.value
+  );
+});
+
+const handleEmailInput = () => {
+  if (emailError.value) validateEmail(email.value);
+  if (validateEmail(email.value)) emailError.value = "";
+};
+
+const handlePasswordInput = () => {
+  if (passwordError.value) validatePassword(password.value);
+  if (validatePassword(password.value)) passwordError.value = "";
+};
+
+const validationClasses = (error: string, inputValue: string) => {
+  return [error ? "error" : "", inputValue && !error ? "success" : ""];
+};
+
+const toggleShowPassword = () => {
+  return passwordInputType.value === "password"
+    ? (passwordInputType.value = "text")
+    : (passwordInputType.value = "password");
+};
+
+const validateForm = () => {
+  return validateEmail(email.value) && validatePassword(password.value);
+};
+
+const resetForm = () => {
+  email.value = "";
+  password.value = "";
+};
+
+const loginUser = async () => {
+  if (!validateForm()) return;
+  try {
+    await login(email.value, password.value);
+    resetForm();
+    router.push("/");
+  } catch (error: any) {
+    loginError.value = error.message;
+  }
+};
 </script>
 
 <template>
-  <section class="login">
+  <form class="login" @submit.prevent="loginUser">
     <h2 class="login__title">Login to have access to your private files.</h2>
     <div class="login__content">
       <div class="login__inputs">
         <BaseInput
-          v-model="email"
+          v-model.trim="email"
           name="email"
           label="Email"
           class="login__email"
+          :class="validationClasses(emailError, email)"
+          @input="handleEmailInput"
+          @blur="validateEmail(email)"
         />
+        <span v-if="emailError" class="error__message">{{ emailError }}</span>
         <BaseInput
-          v-model="password"
+          v-model.trim="password"
+          :type="passwordInputType"
           name="password"
           label="Password"
           class="login__password"
-        />
+          :class="validationClasses(passwordError, password)"
+          @input="handlePasswordInput"
+          @blur="validatePassword(password)"
+        >
+          <button
+            type="button"
+            class="login__password--button"
+            @click="toggleShowPassword"
+          >
+            <EyeIcon />
+          </button>
+        </BaseInput>
+        <span v-if="passwordError" class="error__message">{{
+          passwordError
+        }}</span>
       </div>
       <div class="login__actions">
         <BaseButton label="Cancel" theme="white" to="/" />
-        <BaseButton label="Confirm" />
+        <BaseButton type="submit" label="Confirm" :disabled="!isFormValid" />
       </div>
       <div class="login__redirect">
         <h3 class="login__redirect--title">Don't have an account yet?</h3>
@@ -38,8 +115,11 @@ const password = ref<string>("");
           theme="light-green"
         />
       </div>
+      <span v-if="loginError" class="error__message--action">{{
+        loginError
+      }}</span>
     </div>
-  </section>
+  </form>
 </template>
 
 <style lang="scss" scoped>
@@ -69,6 +149,18 @@ const password = ref<string>("");
   padding-bottom: 2rem;
 }
 
+.login__password--button {
+  border-radius: 8px;
+  position: absolute;
+  right: 7px;
+  top: 32px;
+}
+
+.error.login__email,
+.error.login__password {
+  padding-bottom: 0.2rem;
+}
+
 .login__actions {
   padding: 1rem 0;
   display: flex;
@@ -88,11 +180,39 @@ const password = ref<string>("");
 .login__redirect--link {
   width: 100%;
 }
+
+.error__message {
+  font-size: 0.8rem;
+  color: $text-color-error;
+  display: block;
+  padding-bottom: 0.6rem;
+}
+.error__message--action {
+  font-size: 1.3rem;
+  color: $text-color-error;
+  display: block;
+  text-align: center;
+  padding-bottom: 0.6rem;
+}
 </style>
 
 <style lang="scss">
 .login__email.input__input,
 .login__password.input__input {
   width: 100%;
+}
+
+.error.login__email.input__input,
+.error.login__password.input__input {
+  border: 1px solid $text-color-error;
+}
+
+.success.login__email.input__input,
+.success.login__password.input__input {
+  border: 1px solid $text-color-success;
+}
+
+.login__password {
+  position: relative;
 }
 </style>
