e5188f389fdfd5fef1322c68abc60051e4bc4a9b
Marta Solomacha <marta.solomacha@monterail.com>
2023-09-17 15:52:58 +0200

    Edit useStorage composable

--- composables/useStorage.ts
+++ composables/useStorage.ts
@@ -1,8 +1,10 @@
+import type { StoragePath } from "@/types/StoragePath";
+
 export const useStorage = () => {
   const userData = useUserData();
   const isStorgePublic = useIsStoragePublic();
 
-  const storage = computed(() => {
+  const setStorage = (): StoragePath => {
     if (!userData.value.id || (userData.value.id && isStorgePublic.value)) {
       return {
         bucket: "files",
@@ -14,6 +16,26 @@ export const useStorage = () => {
         folder: userData.value.id,
       };
     }
+  };
+
+  const storage = ref<StoragePath>(setStorage());
+
+  const updateStorage = () => {
+    if (!userData.value.id || (userData.value.id && isStorgePublic.value)) {
+      storage.value.bucket = "files";
+      storage.value.folder = "public";
+    } else {
+      storage.value.bucket = "private";
+      storage.value.folder = userData.value.id;
+    }
+  };
+
+  watch(isStorgePublic, () => {
+    updateStorage();
+  });
+
+  watch(userData, () => {
+    updateStorage();
   });
 
   return { storage };
