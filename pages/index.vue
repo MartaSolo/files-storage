2b225208a9a145d5e6ab90fe0f987cdb27dcd6b0
Marta Solomacha <marta.solomacha@monterail.com>
2023-09-17 22:09:57 +0200

    Remove getSession() from index

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,6 @@
 <script setup lang="ts">
 import { MAX_FILE_SIZE_MB } from "@/utils/constants/maxFileSizeMB";
 import { MAX_FILES_NUMBER } from "@/utils/constants/maxFilesNumber";
-
-const userData = useUserData();
-
-const { retrieveSession } = useRetrieveSession();
-
-const getSessionError = ref("");
-
-const getSession = async () => {
-  try {
-    await retrieveSession();
-  } catch (error: any) {
-    getSessionError.value = error.message;
-  }
-};
-
-onMounted(() => {
-  if (!userData.value.id) getSession();
-});
 </script>
 
 <template>
@@ -27,5 +9,3 @@ onMounted(() => {
     :max-files-number="MAX_FILES_NUMBER"
   />
 </template>
-
-<style lang="css" scoped></style>
