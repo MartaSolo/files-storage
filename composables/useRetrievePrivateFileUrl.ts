682da38580c2b7a97fd44380e35567e608be0cea
Marta Solomacha <marta.solomacha@monterail.com>
2023-09-17 22:10:56 +0200

    Edit useRetrievePrivateFileUrl

--- composables/useRetrievePrivateFileUrl.ts
+++ composables/useRetrievePrivateFileUrl.ts
@@ -1,23 +1,22 @@
 export const useRetrievePrivateFileUrl = (fileName: string) => {
   const client = useSupabaseClient();
+
   const { storage } = useStorage();
 
   const getPrivateUrl = async () => {
     const { data } = await client.storage
       .from(`${storage.value.bucket}`)
       .createSignedUrl(`${storage.value.folder}/${fileName}`, 6000);
-    // return data?.signedURL;
     return data?.signedUrl;
   };
 
-  const { data: url } = useAsyncData(fileName, getPrivateUrl);
-
-  console.log("private storage", storage.value);
-  console.log("private url.value", url.value);
+  const { data: privateUrl } = useAsyncData(fileName, getPrivateUrl, {
+    server: false,
+  });
 
   watch(storage, () => {
     getPrivateUrl();
   });
 
-  return { url };
+  return { privateUrl };
 };
