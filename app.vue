e8b4c8c100a8ba34118cebc26be2ab45dc22e51a
Marta Solomacha <marta.solomacha@monterail.com>
2023-09-19 21:57:27 +0200

    Add getSession onMounted to app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,26 @@
-<template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-</template>
+<script setup lang="ts">
+const { isSessionLoading, retrieveSession } = useRetrieveSession();
 
-<style></style>
+const getSessionError = ref("");
+
+const getSession = async () => {
+  try {
+    await retrieveSession();
+  } catch (error: any) {
+    getSessionError.value = error.message;
+  }
+};
+
+onMounted(() => {
+  getSession();
+});
+</script>
+
+<template>
+  <div>
+    <BaseLoader v-if="isSessionLoading" />
+    <NuxtLayout v-else>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
