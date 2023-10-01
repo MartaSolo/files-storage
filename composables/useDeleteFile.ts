2b9662fd8f4a7aae2863f80e8a4c993a0537ec3c
Marta Solomacha <marta.solomacha@monterail.com>
2023-10-01 14:42:22 +0200

    Edit useDeleteFiles to handle private and public storage

--- composables/useDeleteFile.ts
+++ composables/useDeleteFile.ts
@@ -1,12 +1,16 @@
 export const useDeleteFile = () => {
   const client = useSupabaseClient();
 
+  const { storage } = useStorage();
+
   const remove = async (fileNames: string[]) => {
     const files = fileNames.map((file) => {
-      return `public/${file}`;
+      return `${storage.value.folder}/${file}`;
     });
 
-    const { data, error } = await client.storage.from("files").remove(files);
+    const { data, error } = await client.storage
+      .from(storage.value.bucket)
+      .remove(files);
 
     if (error) {
       throw new Error(error.message);
