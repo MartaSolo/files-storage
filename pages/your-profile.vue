c2c5245ffef0c9cd1a3e91d6456081e8903953e7
Marta Solomacha <marta.solomacha@monterail.com>
2023-10-21 21:21:27 +0200

    Edit your-profile page

--- pages/your-profile.vue
+++ pages/your-profile.vue
@@ -1,34 +1,48 @@
 <script setup lang="ts">
 definePageMeta({
   layout: "login-layout",
-  middleware: "no-auth",
+  // middleware: "no-auth",
 });
 </script>
 
 <template>
   <section class="profile">
+    <h1 class="profile__title">Your personal information</h1>
     <div class="profile__content">
-      <div>your profile - coming soon...</div>
-      <div>
-        <BaseButton to="/">Go to your private storage</BaseButton>
-      </div>
+      <ProfileUpdateForm />
+      <div class="profile__photo">Profile photo</div>
     </div>
+    <!-- <BaseButton to="/">Go to your private storage</BaseButton> -->
   </section>
 </template>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 .profile {
-  display: flex;
-  justify-content: center;
-  padding-top: 150px;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1rem;
+}
+
+.profile__title {
+  font-size: 2rem;
+  text-align: center;
+  padding: 2rem;
+  color: $text-color-secondary;
 }
 
 .profile__content {
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 50px;
-  width: 500px;
+  gap: 2rem;
+  @include mediumScreen {
+    flex-direction: row;
+  }
+}
+
+.profile__photo {
+  background-color: palegreen;
+  @include mediumScreen {
+    flex-basis: 50%;
+  }
 }
 </style>
