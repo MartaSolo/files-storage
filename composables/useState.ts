4f8e33555d5061aaf501acdea5946e6b4752a084
Marta Solomacha <marta.solomacha@monterail.com>
2023-10-22 20:17:32 +0200

    Add useProfileImageSource useState

--- composables/useState.ts
+++ composables/useState.ts
@@ -16,3 +16,8 @@ export const useSelectedFiles = () =>
 
 export const useIsStoragePublic = () =>
   useState("is-storage-public", () => true);
+
+// export const useProfileImageSource = () =>
+//   useState("profile-image-source", () => "https://resvibgtwucmhpdbacog.supabase.co/storage/v1/object/sign/private/1b7a52b7-5877-4c31-9682-f65fea1be952/Marta.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcml2YXRlLzFiN2E1MmI3LTU4NzctNGMzMS05NjgyLWY2NWZlYTFiZTk1Mi9NYXJ0YS5wbmciLCJpYXQiOjE2OTc5OTU2MTksImV4cCI6MTY5ODAwMTYxOX0.NzmJAI5-YNiMx1GzPF7oAZmZSXZpzg2K5z5UrcLjOr4");
+export const useProfileImageSource = () =>
+  useState("profile-image-source", () => "");
