307f498926cfb082be7a4e3c7d70ac389643f08b
Marta Solomacha <marta.solomacha@monterail.com>
2023-10-29 18:57:59 +0100

    Edit useProfileImageSource in useState

--- composables/useState.ts
+++ composables/useState.ts
@@ -1,6 +1,7 @@
 import { FileObjectKeys } from "@/types/FileObjectKeys";
 import { SortOrder } from "@/types/SortOrder";
 import { LayoutType } from "@/types/LayoutType";
+import { PROFILE_PLACEHOLDER_SOURCE } from "@/utils/constants/profilePlaceholderSource";
 
 export const useSortColumn = () =>
   useState<FileObjectKeys>("sort-column", () => "name");
@@ -17,7 +18,5 @@ export const useSelectedFiles = () =>
 export const useIsStoragePublic = () =>
   useState("is-storage-public", () => true);
 
-// export const useProfileImageSource = () =>
-//   useState("profile-image-source", () => "https://resvibgtwucmhpdbacog.supabase.co/storage/v1/object/sign/private/1b7a52b7-5877-4c31-9682-f65fea1be952/Marta.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcml2YXRlLzFiN2E1MmI3LTU4NzctNGMzMS05NjgyLWY2NWZlYTFiZTk1Mi9NYXJ0YS5wbmciLCJpYXQiOjE2OTc5OTU2MTksImV4cCI6MTY5ODAwMTYxOX0.NzmJAI5-YNiMx1GzPF7oAZmZSXZpzg2K5z5UrcLjOr4");
 export const useProfileImageSource = () =>
-  useState("profile-image-source", () => "");
+  useState("profile-image-source", () => PROFILE_PLACEHOLDER_SOURCE);
