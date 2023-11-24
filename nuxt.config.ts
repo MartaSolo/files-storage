a46256d4e92cf59b209b081d468ce3af89f701f8
Marta Solomacha <marta.solomacha@monterail.com>
2023-11-24 19:31:57 +0100

    Add scss partials and update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
       preprocessorOptions: {
         scss: {
           additionalData:
-            "@import '@/assets/scss/_colors.scss'; @import '@/assets/scss/_font.scss'; @import '@/assets/scss/_breakpoints.scss'; @import '@/assets/scss/_mixin.scss';",
+            "@import '@/assets/scss/_colors.scss'; @import '@/assets/scss/_font.scss'; @import '@/assets/scss/_breakpoints.scss'; @import '@/assets/scss/_mixin.scss'; @import '@/assets/scss/_other.scss';",
         },
       },
     },
