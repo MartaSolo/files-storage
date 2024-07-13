78c5f58a42f9a95911a420ccc45ab89b605e6825
Marta Solomacha <marta.solomacha@monterail.com>
2024-07-13 15:52:54 +0200

    Add getSortType utils function

---
+++ utils/helpers/getSortTypes.ts
@@ -0,0 +1,20 @@
+import { FileObject } from "@supabase/storage-js";
+
+export const getSortType = (file?: FileObject, files?: FileObject[]) => {
+  let type: string | string[];
+  if (file) {
+    type = file.metadata.mimetype.split("/")[0];
+  } else if (files) {
+    const allTypes = files.map((file) => {
+      return file.metadata.mimetype.split("/")[0];
+    });
+    const uniqueTypes = allTypes.filter((type, index, array) => {
+      return array.indexOf(type) === index;
+    });
+    type = uniqueTypes;
+  } else {
+    type = "";
+  }
+
+  return type;
+};
