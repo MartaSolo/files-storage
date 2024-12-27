1adc0ec564affae95792c46effce6318701eb2c3
Marta Solomacha <marta.solomacha@monterail.com>
2024-12-27 12:55:26 +0100

    Replace ErrorMessage with BaseMessage

---
+++ components/BaseMessage.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title: string;
+    description?: string;
+    type: "error" | "success" | "info";
+  }>(),
+  {
+    type: "info",
+    description: "",
+  }
+);
+</script>
+
+<template>
+  <div class="message">
+    <h2 :class="['message__title', `message__title--${type}`]">{{ title }}</h2>
+    <p v-if="description" class="message__description">{{ description }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.message {
+  padding: 3rem 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.message__title {
+  font-size: 2.5rem;
+  padding-bottom: 1rem;
+  &--error {
+    color: $text-color-error;
+  }
+  &--success {
+    color: $text-color-success;
+  }
+  &--info {
+    color: $text-color-primary;
+  }
+}
+
+.message__description {
+  font-size: 1.5rem;
+}
+</style>
