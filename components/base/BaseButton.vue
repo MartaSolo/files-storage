75cad6c33c032c84702edd6c26e08e4b31bb4229
Marta Solomacha <marta.solomacha@monterail.com>
2025-03-09 13:34:29 +0100

    Refactor BaseButton

--- components/base/BaseButton.vue
+++ components/base/BaseButton.vue
@@ -1,44 +1,99 @@
 <script setup lang="ts">
-const props = withDefaults(
-  defineProps<{
-    theme?: "white" | "green" | "light-green" | "light-gray";
-    to?: string | null;
-  }>(),
-  {
-    theme: "green",
-    to: null,
-  }
-);
+import { computed } from "vue";
+
+interface Props {
+  theme?: "white" | "green" | "light-green" | "light-gray";
+  to?: string | null;
+  type?: "button" | "submit" | "reset";
+  loading?: boolean;
+  size?: "small" | "medium" | "large";
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  theme: "green",
+  to: null,
+  type: "button",
+  loading: false,
+  size: "medium",
+});
 
 const computedClass = computed(() => {
-  return ["button", `button--${props.theme}`];
+  return [
+    "button",
+    `button--${props.theme}`,
+    `button--${props.size}`,
+    { "button--loading": props.loading },
+  ];
 });
 
 const component = computed(() => {
   return props.to ? resolveComponent("NuxtLink") : "button";
 });
+
+const buttonProps = computed(() => {
+  if (props.to) return { to: props.to };
+  return {
+    type: props.type,
+  };
+});
 </script>
 
 <template>
-  <component :is="component" :to="to" :class="computedClass">
-    <slot></slot
-  ></component>
+  <component
+    :is="component"
+    v-bind="buttonProps"
+    :class="computedClass"
+    :aria-disabled="loading"
+  >
+    <span v-if="loading" class="button__loader"></span>
+    <slot></slot>
+  </component>
 </template>
 
 <style lang="scss" scoped>
 .button {
-  display: flex;
+  display: inline-flex;
   justify-content: center;
   align-items: center;
-  padding: 0.5rem 0.75rem;
   border-radius: 40px;
-  min-width: 90px;
+  font-weight: 500;
+  transition: all 0.2s ease;
+  position: relative;
+  cursor: pointer;
+
   &:disabled {
     cursor: not-allowed;
     opacity: 0.6;
     &:hover {
       opacity: 0.4;
     }
+  }
+
+  &--small {
+    padding: 0.375rem 0.625rem;
+    min-width: 80px;
+    font-size: 0.875rem;
+  }
+
+  &--medium {
+    padding: 0.5rem 0.75rem;
+    min-width: 90px;
+    font-size: 1rem;
+  }
+
+  &--large {
+    padding: 0.625rem 1rem;
+    min-width: 100px;
+    font-size: 1.125rem;
+  }
+
+  &__loader {
+    width: 1em;
+    height: 1em;
+    border: 2px solid currentColor;
+    border-radius: 50%;
+    border-right-color: transparent;
+    animation: button-loading 0.75s linear infinite;
   }
 }
 
@@ -71,6 +126,15 @@ const component = computed(() => {
   color: $text-color-primary;
   &:hover {
     background-color: $color-grey-lighter;
+  }
+}
+
+@keyframes button-loading {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
   }
 }
 </style>
