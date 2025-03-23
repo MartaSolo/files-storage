2a58e22c30a11af94e88290672e100edff838c11
Marta Solomacha <marta.solomacha@monterail.com>
2025-03-23 17:57:27 +0100

    Edit validations for inputs

--- utils/constants/validation.ts
+++ utils/constants/validation.ts
@@ -1,11 +1,44 @@
 export const validation = {
-  nameError:
-    "Name should be at least 2 characters long and contain only letters",
-  nameRegex: /^(?!.* .* )[a-zA-Z ]{2,25}$/,
-  emailError: "Please enter valid email address",
-  emailRegex:
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-  passwordError: "Please enter valid password",
-  passwordRegex:
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/,
+  name: {
+    regex:
+      /^[a-zA-ZÀ-ÿ]+([-']?[a-zA-ZÀ-ÿ]+)*( [a-zA-ZÀ-ÿ]+([-']?[a-zA-ZÀ-ÿ]+)*)*$/,
+    minChar: 2,
+    maxChar: 50,
+    error: {
+      tooShort: "Name must be at least 2 characters long",
+      tooLong: "Name cannot be longer than 50 characters",
+      invalid:
+        "Name can only contain letters, hyphens, apostrophes and single spaces",
+      empty: "Name is required",
+    },
+  },
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    error: {
+      invalid: "Please enter valid email address",
+      empty: "Email is required",
+    },
+  },
+  password: {
+    regex: {
+      digit: /\d/,
+      lowerCase: /[a-z]/,
+      upperCase: /[A-Z]/,
+      specialChar: /[^a-zA-Z0-9]/,
+      noSpace: /^\S*$/,
+    },
+    minChar: 8,
+    maxChar: 15,
+    error: {
+      tooShort: "Password must be at least 8 characters long",
+      tooLong: "Password cannot be longer than 15 characters",
+      noDigit: "Password must contain at least one digit",
+      noLowerCase: "Password must contain at least one lowercase letter",
+      noUpperCase: "Password must contain at least one uppercase letter",
+      noSpecialChar: "Password must contain at least one special character",
+      containsSpace: "Password cannot contain spaces",
+      empty: "Password is required",
+    },
+  },
 };
