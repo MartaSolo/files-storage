64a14fb1d53fdff3e29368f682277d210e58905c
Marta Solomacha <marta.solomacha@monterail.com>
2025-03-23 17:58:32 +0100

    Adjust login, register and my-profile pages

--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,56 @@
+<template>
+  <form class="login" @submit.prevent="loginUser">
+    <h1 class="login__title">Login to have access to your private files.</h1>
+    <div class="login__content">
+      <div class="login__inputs">
+        <BaseInput
+          v-model.trim="email"
+          name="email"
+          label="Email"
+          class="login__email"
+          :error-message="emailError"
+          :valid="emailSuccess"
+          @blur="emailTouched = true"
+        />
+        <BaseInput
+          v-model.trim="password"
+          :type="passwordInputType"
+          name="password"
+          label="Password"
+          class="login__password"
+          :error-message="passwordError"
+          :valid="passwordSuccess"
+          @blur="passwordTouched = true"
+        >
+          <button type="button" class="login__icon" @click="toggleShowPassword">
+            <EyeIcon />
+          </button>
+        </BaseInput>
+      </div>
+      <div class="login__actions">
+        <BaseButton theme="white" to="/">Cancel</BaseButton>
+        <BaseButton type="submit" :disabled="!isFormValid">
+          Confirm
+        </BaseButton>
+      </div>
+      <div class="login__redirect">
+        <h3 class="login__redirect-title">Don't have an account yet?</h3>
+        <BaseButton
+          to="/register"
+          class="login__redirect-link"
+          theme="light-green"
+          size="large"
+        >
+          Register
+        </BaseButton>
+      </div>
+      <span v-if="loginError" class="login__error">
+        {{ loginError }}
+      </span>
+    </div>
+  </form>
+</template>
+
 <script setup lang="ts">
 definePageMeta({
   layout: "no-tabs-layout",
@@ -7,14 +60,18 @@ definePageMeta({
 const EyeIcon = resolveComponent("EyeIcon");
 
 const router = useRouter();
-const { validateEmail, handleEmailInput, emailError } = useValidateEmail();
-const { validatePassword, handlePasswordInput, passwordError } =
-  useValidatePassword();
+const { email, emailTouched, emailError, emailSuccess, isEmailValid } =
+  useValidateEmail();
+const {
+  password,
+  passwordTouched,
+  passwordError,
+  passwordSuccess,
+  isPasswordValid,
+} = useValidatePassword();
 const { login } = useLoginUser();
 const isStoragePublic = useIsStoragePublic();
 
-const email = ref("");
-const password = ref("");
 const passwordInputType = ref("password");
 const loginError = ref("");
 
@@ -27,19 +84,13 @@ const isFormValid = computed(() => {
   );
 });
 
-const validationClasses = (error: string, inputValue: string) => {
-  return [error ? "error" : "", inputValue && !error ? "success" : ""];
-};
-
 const toggleShowPassword = () => {
   return passwordInputType.value === "password"
     ? (passwordInputType.value = "text")
     : (passwordInputType.value = "password");
 };
 
-const validateForm = () => {
-  return validateEmail(email.value) && validatePassword(password.value);
-};
+const validateForm = () => isEmailValid.value && isPasswordValid.value;
 
 const resetForm = () => {
   email.value = "";
@@ -59,143 +110,63 @@ const loginUser = async () => {
 };
 </script>
 
-<template>
-  <form class="login" @submit.prevent="loginUser">
-    <h1 class="login__title">Login to have access to your private files.</h1>
-    <div class="login__content">
-      <div class="login__inputs">
-        <BaseInput
-          v-model.trim="email"
-          name="email"
-          label="Email"
-          class="login__email"
-          :class="validationClasses(emailError, email)"
-          @input="handleEmailInput(email)"
-          @blur="validateEmail(email)"
-        />
-        <span v-if="emailError" class="login__error">{{ emailError }}</span>
-        <BaseInput
-          v-model.trim="password"
-          :type="passwordInputType"
-          name="password"
-          label="Password"
-          class="login__password"
-          :class="validationClasses(passwordError, password)"
-          @input="handlePasswordInput(password)"
-          @blur="validatePassword(password)"
-        >
-          <button
-            type="button"
-            class="login__password--button"
-            @click="toggleShowPassword"
-          >
-            <EyeIcon />
-          </button>
-        </BaseInput>
-        <span v-if="passwordError" class="login__error">{{
-          passwordError
-        }}</span>
-      </div>
-      <div class="login__actions">
-        <BaseButton theme="white" to="/">Cancel</BaseButton>
-        <BaseButton type="submit" :disabled="!isFormValid">
-          Confirm
-        </BaseButton>
-      </div>
-      <div class="login__redirect">
-        <h3 class="login__redirect--title">Don't have an account yet?</h3>
-        <BaseButton
-          to="/register"
-          class="login__redirect--link"
-          theme="light-green"
-          size="large"
-        >
-          Register
-        </BaseButton>
-      </div>
-      <span v-if="loginError" class="login__error--action">
-        {{ loginError }}
-      </span>
-    </div>
-  </form>
-</template>
-
 <style lang="scss" scoped>
 .login {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
 
-.login__title {
-  font-size: 2rem;
-  text-align: center;
-  padding: 2rem;
-  color: $text-color-secondary;
-}
+  &__title {
+    font-size: 2rem;
+    text-align: center;
+    padding: 2rem;
+    color: $text-color-secondary;
+  }
 
-.login__content {
-  width: 100%;
-  max-width: 500px;
-  padding: 0 1rem;
-}
+  &__content {
+    width: 100%;
+    max-width: 500px;
+    padding: 0 1rem;
+  }
 
-.login__email,
-.login__password {
-  flex-direction: column;
-  align-items: flex-start;
-  padding-bottom: 2rem;
-}
+  &__password {
+    position: relative;
+  }
 
-.login__password {
-  position: relative;
-}
+  &__icon {
+    border-radius: 8px;
+    position: absolute;
+    right: 7px;
+    top: 36px;
+  }
 
-.login__password--button {
-  border-radius: 8px;
-  position: absolute;
-  right: 7px;
-  top: 32px;
-}
+  &__actions {
+    padding: 1rem 0;
+    display: flex;
+    justify-content: space-around;
+  }
 
-.error.login__email,
-.error.login__password {
-  padding-bottom: 0.2rem;
-}
+  &__redirect {
+    padding: 2rem 0;
+  }
 
-.login__actions {
-  padding: 1rem 0;
-  display: flex;
-  justify-content: space-around;
-}
+  &__redirect-title {
+    font-size: 1.5rem;
+    padding-bottom: 2rem;
+    text-align: center;
+    color: $text-color-secondary;
+  }
 
-.login__redirect {
-  padding: 2rem 0;
-}
+  &__redirect-link {
+    width: 100%;
+  }
 
-.login__redirect--title {
-  font-size: 1.5rem;
-  padding-bottom: 2rem;
-  text-align: center;
-  color: $text-color-secondary;
-}
-
-.login__redirect--link {
-  width: 100%;
-}
-
-.login__error {
-  font-size: 0.8rem;
-  color: $text-color-error;
-  display: block;
-  padding-bottom: 0.6rem;
-}
-
-.login__error--action {
-  font-size: 1.3rem;
-  color: $text-color-error;
-  display: block;
-  text-align: center;
-  padding-bottom: 0.6rem;
+  &__error {
+    font-size: 1.3rem;
+    color: $text-color-error;
+    display: block;
+    text-align: center;
+    padding-bottom: 0.6rem;
+  }
 }
 </style>
