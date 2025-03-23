669d2b814c26149d5267c7452d5274f3658b3caf
Marta Solomacha <marta.solomacha@monterail.com>
2025-03-23 17:59:06 +0100

    Adjust components to BaseInput changes

--- components/base/BaseMinMaxSlider.vue
+++ components/base/BaseMinMaxSlider.vue
@@ -1,3 +1,62 @@
+<template>
+  <div class="slider">
+    <p class="slider__label">{{ label }}</p>
+    <div class="slider__range minmax">
+      <input
+        id="min"
+        type="range"
+        name="min"
+        :min="min"
+        :max="max"
+        :value="minValue"
+        :step="step"
+        @input="onInputMin"
+      />
+      <input
+        id="max"
+        type="range"
+        name="max"
+        :min="min"
+        :max="max"
+        :value="maxValue"
+        :step="step"
+        @input="onInputMax"
+      />
+    </div>
+    <div class="slider__inputs">
+      <div class="slider__input">
+        <BaseInput
+          name="min-size"
+          type="number"
+          class="slider__input--input"
+          :min="min"
+          :max="max"
+          :model-value="minValue"
+          :step="step"
+          @input="onInputMin"
+        >
+          <span v-if="unit" class="slider__input--unit">{{ unit }}</span>
+        </BaseInput>
+      </div>
+      <div class="slider__dash"></div>
+      <div class="slider__input">
+        <BaseInput
+          name="max-size"
+          type="number"
+          class="slider__input--input"
+          :min="min"
+          :max="max"
+          :model-value="maxValue"
+          :step="step"
+          @input="onInputMax"
+        >
+          <span v-if="unit" class="slider__input--unit">{{ unit }}</span>
+        </BaseInput>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
 const props = withDefaults(
   defineProps<{
@@ -42,163 +101,108 @@ const onInputMax = (e: Event) => {
 };
 </script>
 
-<template>
-  <div class="slider">
-    <p class="slider__label">{{ label }}</p>
-    <div class="slider__range minmax">
-      <input
-        id="min"
-        type="range"
-        name="min"
-        :min="min"
-        :max="max"
-        :value="minValue"
-        :step="step"
-        @input="onInputMin"
-      />
-      <input
-        id="max"
-        type="range"
-        name="max"
-        :min="min"
-        :max="max"
-        :value="maxValue"
-        :step="step"
-        @input="onInputMax"
-      />
-    </div>
-    <div class="slider__inputs">
-      <div class="slider__input">
-        <BaseInput
-          name="min-size"
-          type="number"
-          class="slider__input--input"
-          :min="min"
-          :max="max"
-          :model-value="minValue"
-          :step="step"
-          @input="onInputMin"
-        >
-          <span v-if="unit" class="slider__input--unit">{{ unit }}</span>
-        </BaseInput>
-      </div>
-      <div class="minmax__dash"></div>
-      <div class="slider__input">
-        <BaseInput
-          name="max-size"
-          type="number"
-          class="slider__input--input"
-          :min="min"
-          :max="max"
-          :model-value="maxValue"
-          :step="step"
-          @input="onInputMax"
-        >
-          <span v-if="unit" class="slider__input--unit">{{ unit }}</span>
-        </BaseInput>
-      </div>
-    </div>
-  </div>
-</template>
-
 <style lang="scss" scoped>
-.slider__label {
-  padding-bottom: 0.75rem;
-}
-
-.slider__range {
+.slider {
   --trackHeight: 0.5rem;
   --thumbRadius: 1rem;
-}
 
-/* style for the input element with type "range" */
-.slider__range input[type="range"] {
-  position: relative;
-  appearance: none;
-  background: none;
-  pointer-events: none;
-  border-radius: 999px;
-  z-index: 0;
-}
-
-/* ::before element to replace the slider track */
-.slider__range input[type="range"]::before,
-.slider__range.minmax::before {
-  content: "";
-  position: absolute;
-  height: 100%;
-  background: $color-green-dark;
-  pointer-events: none;
-  border-radius: 999px;
-}
-
-.slider__range input[type="range"]::-webkit-slider-thumb {
-  position: relative;
-  width: var(--thumbRadius);
-  height: var(--thumbRadius);
-  margin-top: calc((var(--trackHeight) - var(--thumbRadius)) / 2);
-  background: $color-green-dark;
-  border-radius: 999px;
-  pointer-events: all;
-  appearance: none;
-  z-index: 1;
-}
-
-.slider__range.minmax {
-  position: relative;
-  height: var(--trackHeight);
-  background: $color-green-medium;
-  border-radius: 999px;
-  margin: 0.5rem 0;
-  --progressLeft: 0%;
-  --progressRight: 0%;
-}
-
-.slider__range.minmax input[type="range"] {
-  position: absolute;
-  pointer-events: none;
-  width: 100%;
-}
-
-.slider__range.minmax input[type="range"]::-webkit-slider-runnable-track {
-  background: none;
-}
-
-.slider__range.minmax::before {
-  left: v-bind(leftPercent);
-  right: v-bind(rightPercent);
-  width: unset;
-}
-
-.slider__range.minmax input[type="range"]::before {
-  display: none;
-}
-
-.slider__inputs {
-  padding-top: 0.5rem;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.minmax__dash {
-  width: 15px;
-  height: 2px;
-  border-radius: 8px;
-  background-color: $text-color-secondary;
-  @include smallScreen {
-    width: 30px;
+  &__label {
+    padding-bottom: 0.75rem;
   }
-}
 
-.slider__input--unit {
-  padding: 0.5rem 0 0.5rem 0.5rem;
-}
+  &__range input[type="range"] {
+    position: relative;
+    appearance: none;
+    background: none;
+    pointer-events: none;
+    border-radius: 999px;
+    z-index: 0;
+  }
 
-.slider__input {
-  width: 100px;
-  @include smallScreen {
-    width: 150px;
+  /* ::before element to replace the slider track */
+  &__range input[type="range"]::before,
+  &__range.minmax::before {
+    content: "";
+    position: absolute;
+    height: 100%;
+    background: $color-green-dark;
+    pointer-events: none;
+    border-radius: 999px;
+  }
+
+  &__range input[type="range"]::-webkit-slider-thumb {
+    position: relative;
+    width: var(--thumbRadius);
+    height: var(--thumbRadius);
+    margin-top: calc((var(--trackHeight) - var(--thumbRadius)) / 2);
+    background: $color-green-dark;
+    border-radius: 999px;
+    pointer-events: all;
+    appearance: none;
+    z-index: 1;
+  }
+
+  &__range.minmax {
+    position: relative;
+    height: var(--trackHeight);
+    background: $color-green-medium;
+    border-radius: 999px;
+    margin: 0.5rem 0;
+    --progressLeft: 0%;
+    --progressRight: 0%;
+  }
+
+  &__range.minmax input[type="range"] {
+    position: absolute;
+    pointer-events: none;
+    width: 100%;
+  }
+
+  &__range.minmax input[type="range"]::-webkit-slider-runnable-track {
+    background: none;
+  }
+
+  &__range.minmax::before {
+    left: v-bind(leftPercent);
+    right: v-bind(rightPercent);
+    width: unset;
+  }
+
+  &__range.minmax input[type="range"]::before {
+    display: none;
+  }
+
+  &__inputs {
+    padding-top: 0.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__dash {
+    width: 15px;
+    height: 2px;
+    border-radius: 8px;
+    background-color: $text-color-secondary;
+    @include smallScreen {
+      width: 30px;
+    }
+  }
+
+  &__input--unit {
+    padding: 0.5rem 0 0.5rem 0.5rem;
+  }
+
+  &__input {
+    width: 100px;
+    @include smallScreen {
+      width: 120px;
+    }
+  }
+
+  &__input--input {
+    flex-direction: row;
+    margin-bottom: 0;
   }
 }
 </style>
