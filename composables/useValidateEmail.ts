bac3a0309b17e69d03842c6ed6efa92dc6df0f97
Marta Solomacha <marta.solomacha@monterail.com>
2025-03-23 17:58:08 +0100

    Change validation composables for inputs

--- composables/useValidateEmail.ts
+++ composables/useValidateEmail.ts
@@ -1,25 +1,33 @@
 import { validation } from "@/utils/constants/validation";
 
 export const useValidateEmail = () => {
+  const email = ref("");
   const emailError = ref("");
   const emailTouched = ref(false);
+  const emailSuccess = ref(false);
 
-  const validateEmail = (email: string) => {
-    emailTouched.value = true;
+  watch([email, emailTouched], () => {
+    if (!emailTouched.value) return;
 
-    if (!validation.emailRegex.test(email)) {
-      emailError.value = validation.emailError;
-    } else {
-      emailError.value = "";
+    if (!email.value) {
+      emailError.value = validation.email.error.empty;
+      emailSuccess.value = false;
+      return;
     }
-    return validation.emailRegex.test(email);
-  };
 
-  const handleEmailInput = (email: string) => {
-    if (emailTouched.value && emailError.value) {
-      validateEmail(email);
+    if (!validation.email.regex.test(email.value)) {
+      emailError.value = validation.email.error.invalid;
+      emailSuccess.value = false;
+      return;
     }
-  };
 
-  return { validateEmail, handleEmailInput, emailError };
+    emailSuccess.value = true;
+    emailError.value = "";
+  });
+
+  const isEmailValid = computed(
+    () => email.value && emailTouched.value && !emailError.value
+  );
+
+  return { email, emailTouched, emailError, emailSuccess, isEmailValid };
 };
--- composables/useValidateName.ts
+++ composables/useValidateName.ts
@@ -1,26 +1,45 @@
 import { validation } from "@/utils/constants/validation";
 
 export const useValidateName = () => {
+  const name = ref("");
   const nameError = ref("");
   const nameTouched = ref(false);
+  const nameSuccess = ref(false);
 
-  const validateName = (name: string) => {
-    nameTouched.value = true;
+  watch([name, nameTouched], () => {
+    if (!nameTouched.value) return;
 
-    if (!validation.nameRegex.test(name)) {
-      nameError.value = validation.nameError;
-    } else {
-      nameError.value = "";
+    if (!name.value) {
+      nameError.value = validation.name.error.empty;
+      nameSuccess.value = false;
+      return;
     }
 
-    return validation.nameRegex.test(name);
-  };
-
-  const handleNameInput = (name: string) => {
-    if (nameTouched.value && nameError.value) {
-      validateName(name);
+    if (name.value.length < validation.name.minChar) {
+      nameError.value = validation.name.error.tooShort;
+      nameSuccess.value = false;
+      return;
     }
-  };
 
-  return { validateName, handleNameInput, nameError };
+    if (name.value.length > validation.name.maxChar) {
+      nameError.value = validation.name.error.tooLong;
+      nameSuccess.value = false;
+      return;
+    }
+
+    if (!validation.name.regex.test(name.value)) {
+      nameError.value = validation.name.error.invalid;
+      nameSuccess.value = false;
+      return;
+    }
+
+    nameSuccess.value = true;
+    nameError.value = "";
+  });
+
+  const isNameValid = computed(
+    () => name.value && nameTouched.value && !nameError.value
+  );
+
+  return { name, nameTouched, nameError, nameSuccess, isNameValid };
 };
