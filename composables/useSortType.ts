601f44002c84000401e745786210eb86cc386930
Marta Solomacha <marciamarcia@o2.pl>
2026-08-28 23:48:14 +0200

    Remove useSortType and use getSortType instead

--- composables/useSortType.ts
+++
@@ -1,7 +0,0 @@
-import type { FileObject } from "@supabase/storage-js";
-import { getSortType } from "@/utils/helpers/getSortTypes";
-
-export const useSortType = (input: FileObject | FileObject[]) => {
-  const type = ref<string[]>(getSortType(input));
-  return { type };
-};
